bdd2330d1167c749a53b1fcd35fca30ce6de77ae
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-07 12:54:10 +0300

    feat: Configure Django static files directory

--- Django_Level_One/first_project/first_project/settings.py
+++ Django_Level_One/first_project/first_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR /'templates'
+STATIC_DIR = BASE_DIR /'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
