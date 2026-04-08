02e34595f323c0a7294b700c965aff063c3c909d
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-08 18:39:19 +0300

    feat: Create new Django project 'ProTwo' and 'appTwo'

---
+++ Django_Level_Two/ProTwo/appTwo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, null=False, unique=True)
