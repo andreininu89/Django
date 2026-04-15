5d2cb709f8f938dd2a7c5a8ba125470e49fae654
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-15 10:38:11 +0300

    Setting up Forms and Models for Users

--- Django_Level_Five/learning_users/basic_app/models.py
+++ Django_Level_Five/learning_users/basic_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User)
+
+    # Add any additional attributes you want
+    portfolio = models.URLField(max_length=200, blank=True)
+    picture = models.ImageField(upload_to="profile_pics/", blank=True)
+
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User!
+        return self.user.username
