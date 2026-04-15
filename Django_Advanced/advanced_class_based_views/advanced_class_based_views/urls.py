dec4791a6f1e1dd9cc4111392b25e1e53068b734
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-15 19:54:11 +0300

    Finished basic class based view

--- Django_Advanced/advanced_class_based_views/advanced_class_based_views/urls.py
+++ Django_Advanced/advanced_class_based_views/advanced_class_based_views/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from basic_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.CBView.as_view()),
+    path("basic_app/", include("basic_app.urls")),
 ]
