7222b63337efa51314035c046cca2813f0fe32e2
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-15 20:25:00 +0300

    Using TemplateView to inject context into index.html

--- Django_Advanced/advanced_class_based_views/basic_app/views.py
+++ Django_Advanced/advanced_class_based_views/basic_app/views.py
@@ -11,3 +11,8 @@ class CBView(View):
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["injectme"] = "Basic Injection!"
+        return context
