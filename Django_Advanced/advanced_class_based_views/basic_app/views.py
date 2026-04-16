ba27e71b40960def656587178c3e316274f367bb
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-16 11:13:49 +0300

    Preparing project for ListView and DetailView

--- Django_Advanced/advanced_class_based_views/basic_app/views.py
+++ Django_Advanced/advanced_class_based_views/basic_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
 
 
